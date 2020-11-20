e32c1cc8d60f5dbf373e7f5a33751e5c8a997b9c
Rainson <Rainson@RENE-PC>
2020-11-20 09:33:50 +0100

    first commit

---
+++ PluralsightsDownloader/PluralSightsCourseData.cs
@@ -0,0 +1,133 @@
+﻿using PluralsightsDownloader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluralsightsDownloader
+{
+    class PluralSightsCourseData
+    {
+
+        public class SkillPath
+        {
+            public string id { get; set; }
+            public string title { get; set; }
+            public bool retired { get; set; }
+            public string url { get; set; }
+            public string urlSlug { get; set; }
+            public string iconUrl { get; set; }
+        }
+
+        public class Retired
+        {
+            public bool isRetired { get; set; }
+            public string reason { get; set; }
+            public string replacementId { get; set; }
+        }
+
+        public class Rating
+        {
+            public double average { get; set; }
+            public int ratersCount { get; set; }
+        }
+
+        public class Clip
+        {
+            public string id { get; set; }
+            public string title { get; set; }
+            public string duration { get; set; }
+            public string playerUrl { get; set; }
+        }
+
+        public class Module
+        {
+            public string id { get; set; }
+            public string title { get; set; }
+            public string duration { get; set; }
+            public string playerUrl { get; set; }
+            public List<Clip> clips { get; set; }
+        }
+
+        public class CourseImage
+        {
+            public object courseListUrl { get; set; }
+            public object defaultUrl { get; set; }
+            public object smallUrl { get; set; }
+        }
+
+        public class Author
+        {
+            public string id { get; set; }
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string displayName { get; set; }
+        }
+
+        public class RootObject
+        {
+            public string id { get; set; }
+            public string publishedOn { get; set; }
+            public string updatedOn { get; set; }
+            public string title { get; set; }
+            public string shortDescription { get; set; }
+            public string description { get; set; }
+            public string level { get; set; }
+            public string duration { get; set; }
+            public int popularityScore { get; set; }
+            public bool hasTranscript { get; set; }
+            public bool hasLearningCheck { get; set; }
+            public string playerUrl { get; set; }
+            public List<SkillPath> skillPaths { get; set; }
+            public Retired retired { get; set; }
+            public Rating rating { get; set; }
+            public List<Module> modules { get; set; }
+            public CourseImage courseImage { get; set; }
+            public List<Author> authors { get; set; }
+            public List<string> audiences { get; set; }
+        }
+    }
+
+    public class DownloadData
+    {
+        public class Url
+        {
+            public int rank { get; set; }
+            public string cdn { get; set; }
+            public string url { get; set; }
+        }
+
+        public class RootObject
+        {
+            public List<Url> urls { get; set; }
+        }
+    }
+
+    public class RPC
+    {
+
+        public class Course
+        {
+            public string title { get; set; }
+            public string name { get; set; }
+            public bool courseHasCaptions { get; set; }
+            public bool supportsWideScreenVideoFormats { get; set; }
+            public string timestamp { get; set; }
+        }
+
+        public class Payload
+        {
+            public Course course { get; set; }
+        }
+
+        public class RootObject
+        {
+            
+            public Payload payload { get; set; }
+            
+        }
+
+}
+}
+
---
+++ PluralsightsDownloader/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PluralsightsDownloader
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            HttpClient client = new HttpClient();
+            FormUrlEncodedContent content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("RedirectUrl", "/"),
+                new KeyValuePair<string, string>("Username", ""),
+                new KeyValuePair<string, string>("Password", ""),
+                new KeyValuePair<string, string>("ShowCaptcha", "False"),
+                new KeyValuePair<string, string>("ReCaptchaSiteKey", "6LeVIgoTAAAAAIhx_TOwDWIXecbvzcWyjQDbXsaV") // copied this from the website .. dont know whether this ever changes
+            });
+            var result = client.PostAsync("https://app.pluralsight.com/id/", content).Result;
+            var resultContent = result.Content.ReadAsStringAsync().Result;
+            if (resultContent.Contains("currentUser"))
+            {
+                // logged in
+                DownloadCourse(client, "patterns-library");
+            }
+        }
+
+        static void DownloadCourse(HttpClient client, string courseName)
+        {
+            
+            var result = client.GetAsync("https://app.pluralsight.com/learner/content/courses/" + courseName).Result;
+            var resultContent = result.Content.ReadAsStringAsync().Result;
+            var resultData = Newtonsoft.Json.JsonConvert.DeserializeObject<PluralSightsCourseData.RootObject>(resultContent);
+
+            string basePath = resultData.title.Replace(":","");
+            Directory.CreateDirectory(basePath);
+
+            var boundries = getVideoBoundries(client, courseName);
+            for (int m = 23; m < resultData.modules.Count; m++)
+            {
+                string path = basePath+ "\\" + (m+1) + ". " + resultData.modules[m].title;
+                Directory.CreateDirectory(path);
+                
+                for (int c = 0; c < resultData.modules[m].clips.Count; c++)
+                {
+                    DownloadClip(client, resultData.modules[m].clips[c], path  +"\\" + (c+1) + ". " + resultData.modules[m].clips[c].title +".mp4", boundries);
+                    for (int i = 0; i < 60; i++)
+                    {
+                        System.Threading.Thread.Sleep(1000);
+                        Console.WriteLine("Wait " + (60-i) + " Seconds");
+                    }
+                    
+                }
+            }
+        }
+
+        private static string getVideoBoundries(HttpClient client, string courseName)
+        {
+            
+            var httpContent = new StringContent("{\"fn\":\"bootstrapPlayer\",\"payload\":{\"courseId\":\""+ courseName + "\"}}", Encoding.UTF8, "application/json");
+            var result = client.PostAsync("https://app.pluralsight.com/player/functions/rpc", httpContent).Result;
+            var resultContent = result.Content.ReadAsStringAsync().Result;
+            var data = Newtonsoft.Json.JsonConvert.DeserializeObject<RPC.RootObject>(resultContent);
+            return data.payload.course.supportsWideScreenVideoFormats ? "1280x720" : "1024x768";
+        }
+
+        static void DownloadClip(HttpClient client, PluralSightsCourseData.Clip clip, string localFilePath, string boundries)
+        {
+
+            var id = clip.id;
+            string courseName = id.Split('|')[0];
+            string author = id.Split('|')[1];
+            string module = id.Split('|')[2];
+            string clipName = id.Split('|')[3];
+            string clipIndex = clip.playerUrl;
+            clipIndex = clipIndex.Substring(clipIndex.IndexOf("clip=") + 5);
+            clipIndex = clipIndex.Substring(0, clipIndex.IndexOf("&"));
+            
+            var httpContent = new StringContent("{\"author\":\""+ author + "\",\"includeCaptions\":false,\"clipIndex\":"+ clipIndex + ",\"courseName\":\"" + courseName + "\",\"locale\":\"en\",\"moduleName\":\""+ module + "\",\"mediaType\":\"mp4\",\"quality\":\""+ boundries + "\"}", Encoding.UTF8, "application/json");
+            var result = client.PostAsync("https://app.pluralsight.com/video/clips/viewclip", httpContent).Result;
+            var resultContent = result.Content.ReadAsStringAsync().Result;
+            var data = Newtonsoft.Json.JsonConvert.DeserializeObject<DownloadData.RootObject>(resultContent);
+            var file = client.GetByteArrayAsync(data.urls[0].url).Result;
+            File.WriteAllBytes(localFilePath.Replace("?","").Replace(":","").Replace("w/","").Replace("/",""), file);
+        }
+        //static void OpenCourse(HttpClient client, string courseUrl)
+        //{
+        //    var result = client.GetAsync(courseUrl).Result;
+        //    var resultContent = result.Content.ReadAsStringAsync().Result;
+        //    var json = resultContent.Substring(resultContent.IndexOf("<script type=\"text/javascript\">__INITIAL_STATE__ = ") + "<script type=\"text/javascript\">__INITIAL_STATE__ = ".Length);
+        //    json = json.Substring(0, json.IndexOf("</script>"));
+
+        //}
+    }
+}
